a0e332e3e45c429ac4c937355902ff81ef005d44
Hidgik <Egor-655@yandex.ru>
2022-03-23 23:24:13 +0500

    Initial commit

--- main.py
+++ main.py
@@ -21,54 +21,32 @@ def handle_text(message):
     buttons = ["IP адрес", "email", "Телефоны"]
     keyboard.add(*buttons)
     bot.send_message(message.chat.id, 'Выбери, что искать',  reply_markup=keyboard)
-    bot.register_next_step_handler(message, choi)
+    bot.register_next_step_handler(message, choice_search)
 
 
-def choi(message):
+def choice_search(message):
     #if message.text.strip() == 'Телефоны':
     regular = RegularResource()
     bot.send_message(message.chat.id, 'Напиши запрос, по которому я буду искать', reply_markup=telebot.types.ReplyKeyboardRemove())
-    bot.register_next_step_handler(message, ge, regular.get_one_regular(message.text.strip()))
+    bot.register_next_step_handler(message, get_results, regular.get_one_regular(message.text.strip()))
 
 
-def ge(message, regular):
+def get_results(message, regular):
     query = message.text.strip()
     user = Users(id=message.chat.id, query=query, regular=regular)
     user.save(force_insert=True)
 
 
-def te():
+def main_threading():
     while True:
         for user in Users.select():
-            th = Thread(target=sea, args=(user.id, user.query, user.regular))
+            th = Thread(target=threading, args=(user.id, user.query, user.regular))
             th.start()
-            # query, regular = user.query, user.regular
-            # links = []
-            # for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
-            #     try:
-            #         links.append(j)
-            #     except Exception:
-            #         pass
-
-            # for i in links:
-            #     try:
-            #         uf = urllib.request.urlopen(i)
-            #         html = uf.read()
-            #         items = [
-            #             m.group()
-            #             for m in re.finditer(regular, html.decode('utf-8'))
-            #         ]
-            #         if items:
-            #             bot.send_message(user.id, i, reply_markup=telebot.types.ReplyKeyboardRemove())
-            #             bot.send_message(user.id, ", ".join(list(set(items))), reply_markup=telebot.types.ReplyKeyboardRemove())
-            #     except Exception:
-            #         pass
-            # bot.send_message(user.id, "Готово", reply_markup=telebot.types.ReplyKeyboardRemove())
             us = Users.get(Users.id == user.id)
             us.delete_instance()
 
 
-def sea(idd, query, regular):
+def threading(idd, query, regular):
     links = []
     for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
         try:
@@ -91,7 +69,7 @@ def sea(idd, query, regular):
     bot.send_message(idd, "Готово", reply_markup=telebot.types.ReplyKeyboardRemove())
 
 
-th = Thread(target=te, args=())
+th = Thread(target=main_threading, args=())
 th.start()
 
 bot.enable_save_next_step_handlers(delay=2)
